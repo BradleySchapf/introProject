4c66198ae53f296ddf972f1ebd71f140f2628bd5
BradleySchapf <bradleyschapf@icloud.com>
2024-11-10 21:38:31 +0100

    updated tests

--- test/controllers/move_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class MoveControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get move_show_url
-    assert_response :success
-  end
-end
--- test/controllers/types_controller_test.rb
+++ test/controllers/types_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
-class TypeControllerTest < ActionDispatch::IntegrationTest
+class TypesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get type_index_url
+    get types_index_url
     assert_response :success
   end
 end
