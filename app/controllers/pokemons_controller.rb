2b3bc676f7cd0e6e5f59716436a79b0abe6ffc89
BradleySchapf <bradleyschapf@icloud.com>
2024-11-19 15:34:26 +0100

    added simple search

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,6 +1,10 @@
 class PokemonsController < ApplicationController
   def index
-    @pokemons = Pokemon.all
+    if params[:query].present?
+      @pokemons = Pokemon.where("LOWER(name) LIKE ?", "%#{params[:query].downcase}%")
+    else
+      @pokemons = Pokemon.all
+    end
   end
 
   def show
